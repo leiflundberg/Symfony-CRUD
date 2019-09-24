2a4bc1048e529f576f44ce0bd5e105a658f8db67
Leif Lundberg <145433@usn.no>
2019-09-24 12:10:59 +0200

    Save function in ArticleController added

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,22 +1,39 @@
 <?php
   namespace App\Controller;
 
+  use App\Entity\Article;
+
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-  class ArticleController extends Controller {
-      /**
-       * @Route("/")
-       * @Method({"GET"})
-       */
-      public function index() {
-          //return new response('<html><body>Hello</body></html>');
+  class ArticleController extends Controller 
+  {
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
+    public function index() 
+    {
+        $articles = ['Article 1', 'Article 2'];
 
-          $articles = ['Article 1', 'Article 2'];
+        return $this->render('articles/index.html.twig', array('articles' => $articles));
+    }
 
-          return $this->render('articles/index.html.twig', array('articles' => $articles));
-      }
+    /**
+     * @Route("/article/save")
+     * 
+     */
+    public function save() 
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = new article(); 
+        $article->setTitle('Article one');
+        $article->setBody('This is the body for article one');
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new Response('Saved an article with the id of '.$article->getId()); 
+    }
   }
-
