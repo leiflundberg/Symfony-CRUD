2731812ee5f84b818758f81a76a7753ef24a255e
Leif Lundberg <145433@usn.no>
2019-09-24 14:58:03 +0200

    Route show added and articles now pulls from mysql database instead of hard coded

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,29 +11,40 @@
   class ArticleController extends Controller 
   {
     /**
-     * @Route("/")
+     * @Route("/", name="article_list")
      * @Method({"GET"})
      */
     public function index() 
     {
-        $articles = ['Article 1', 'Article 2'];
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
 
     /**
-     * @Route("/article/save")
-     * 
+     * @Route("/article/{id}", name="article_show")
      */
-    public function save() 
+    public function show($id)
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        $article = new article(); 
-        $article->setTitle('Article one');
-        $article->setBody('This is the body for article one');
-        $entityManager->persist($article);
-        $entityManager->flush();
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
-        return new Response('Saved an article with the id of '.$article->getId()); 
+        return $this->render('articles/show.html.twig', array('article' => $article));
     }
+
+
+    // /**
+    //  * @Route("/article/save")
+    //  * 
+    //  */
+    // public function save() 
+    // {
+    //     $entityManager = $this->getDoctrine()->getManager();
+    //     $article = new article(); 
+    //     $article->setTitle('Article one');
+    //     $article->setBody('This is the body for article one');
+    //     $entityManager->persist($article);
+    //     $entityManager->flush();
+
+    //     return new Response('Saved an article with the id of '.$article->getId()); 
+    // }
   }
